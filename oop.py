d79fa34707ed5549bb3909293497f00c04e404a1
عبدالعزيز ناصر العديل <abduluziz37@gmail.com>
2026-07-17 14:55:43 +0300

    Add files via upload

--- oop.py
+++ oop.py
@@ -2,35 +2,41 @@ from tkinter import Tk, Label, Button, StringVar
 from tkinter.ttk import Progressbar, Combobox
 from tkinter.filedialog import askopenfile
 from tkinter.messagebox import showinfo, showerror
-from contextlib import redirect_stderr
-import os
-import subprocess
-import re
-import queue
+from threading import Thread
 from time import sleep 
-
+from contextlib import redirect_stderr
 from demucs.pretrained import get_model
 from demucs.apply import apply_model
+
+import os
+import subprocess
+import queue
+import re
 import ffmpeg
 import torch
 import torchaudio
-from threading import Thread
 
 # Constants
 WINDOW_BG = '#222222'
 BTN_BG = '#0E6CB4'
 FG = 'white'
+BTN_STYLE = {'bg': BTN_BG, 'fg': FG}
 BTN_PACKING = {'ipadx': 20, 'ipady': 5}
 SUPPORTED_VIDEO_FORMATS = '*.MP4 *.MKV *.MOV *.WEBM *.AVI *.FLV'
 
 class ProgressRecorder():
+    '''
+        If you want to customize redirect_stderr/redirect_stdout,\n
+        you have to make a class with two methods: `write(text)`, `flush()`.\n
+        `text` will contain the stderr/stdout; So you can deal with it as you want.
+    '''
     def setup(self, app):
         self.app = app
 
     def write(self, text):
-        match = re.search(r'(\d+)%', text)
+        match = re.search(r'(\d+)%', text) # search for a number followed by %.
 
-        if match:
+        if match: 
             percentage = int(match.group(1))
             self.app.guiding_label.config(text=f'{percentage}%\n{self.app.last_guiding_text}')
             self.app.progress_bar.config(value=percentage)
@@ -54,6 +60,7 @@ class ClipsMusicRemover:
         self.name_without_format = self.video_name[:self.video_name.find('.')]
         self.audio_name = self.name_without_format + '.wav'
 
+        # here the processing will start
         self.slice_video_into_clips()
         self.watch_while_process()
         
@@ -109,8 +116,8 @@ class ClipsMusicRemover:
             dimesion_args = ['-fs']
         else:
             dimesion_args = [
-                '-x', self.clip_dimensions.split('x')[1],
-                '-y', self.clip_dimensions.split('x')[0],
+                '-x', self.clip_dimensions.split('x')[0],
+                '-y', self.clip_dimensions.split('x')[1],
             ]
 
         while True:
@@ -188,7 +195,8 @@ class VideoMusicRemover:
         self.name_cases = name_cases
         self.name_without_format = self.video_name[:self.video_name.find('.')]
         self.audio_name = self.name_without_format + '.wav'
-
+        
+        # here the processing will start
         self.extract_audio()
         self.separate_music()
         self.merge_media()
@@ -223,6 +231,8 @@ class VideoMusicRemover:
 
     def merge_media(self):
         self.app.gui_preparation('merge media')
+
+        # that prompt will replace the old polluted audio from the video with the pure one.
         process = subprocess.Popen([
             'ffmpeg',
             '-i', self.video_name,
@@ -231,7 +241,7 @@ class VideoMusicRemover:
             '-map', '1:a:0',
             '-c:v', 'copy',
             '-c:a', 'aac',
-            self.name_cases['music removed'],
+            self.name_cases['music removed'], # the output video name
             '-y'
         ])
         process.wait()
@@ -245,69 +255,83 @@ class App(Tk):
 
         self.title('مزيل الموسيقا')
         self.config(bg=WINDOW_BG)
-        self.geometry('300x300')
+        self.geometry('300x320')
+        self.resizable(False, False)
 
         self.guiding_label = Label(self, text='الرجاء تحديد المقطع لبدء المعالجة', bg=WINDOW_BG, fg=FG)
-        self.guiding_label.pack(self.label_packing) 
+        self.guiding_label.pack(self.label_packing)
 
-        self.select_btn = Button(self, text='اختر المقطع', bg=BTN_BG, fg=FG, command=self.select_video)
+        self.select_btn = Button(self, BTN_STYLE, text='اختر المقطع', command=self.select_video)
         self.select_btn.pack(ipadx=20, ipady=5)
 
         self.progress_bar = Progressbar(self, length=200, orient='horizontal')
-
     
-    def setup(self, progress_recorder, vmr, cmr):
+    def setup(self, progress_recorder: ProgressRecorder, vmr: VideoMusicRemover, cmr: ClipsMusicRemover):
+        '''
+        It connects every related class with the `App` class.\n
+        Basically, it makes a setup.
+        '''
         self.progress_recorder = progress_recorder
         self.vmr = vmr
         self.cmr = cmr
 
     def select_video(self):
+        '''
+        It manages video selection, handles the `AttributeError` if the video\n
+        hasn't been selected.
+        '''
         try:
             self.video_name = askopenfile(filetypes=[('مقاطع الفيديو', SUPPORTED_VIDEO_FORMATS)]).name
         except AttributeError:
             showinfo('ملاحظة', 'لم يتم اختيار المقطع')
         else:
+            # underscored_name 'underscores' the file name only.
             self.underscored_name = self.video_name[:self.video_name.rfind('/')] + '_'.join(self.video_name[self.video_name.rfind('/'):].split())
             self.name_cases['original'] = self.video_name
             self.name_cases['underscored'] = self.underscored_name
             self.name_cases['music removed'] = self.video_name[:self.video_name.find('.')] + ' (بلا موسيقا).mp4'
 
-            try:
-                self.video_name = self.rename_video_to('underscored')
-            except FileExistsError:
-                print('\n\n\nIt seems the filename is already underscored\n\n\n')
+            # so it doesn't show FileNotFoundError when the filename has spaces
+            self.video_name = self.rename_video_to('underscored')
 
-            self.select_btn.forget()
-            self.update_guiding_label('المشاهدة الفورية بلا موسيقا تعرض لك المقطع\n.بصورة لقطات فور الانتهاء من تنقية كل لقطة\n\n' \
-                                      'أما حفظ المقطع بلا موسيقا فيكون بتنقيته\n.ثم حفظه في الجهاز بكل بساطة')        
             self.show_processing_options()
 
     def show_processing_options(self):
-        self.direct_display_btn = Button(
-            self,
-            text='مشاهدة فورية بلا موسيقا',
-            bg=BTN_BG,
-            fg=FG,
-            command=lambda: self.go_to_option('show')
-        )
+        '''
+        It gives the user options how to process the video either by saving the whole video\n
+        without music, or by watching it pure through clean clips.
+        '''
+
+        self.select_btn.forget()
+        self.label_packing['pady'] = (30, 30)
+        # describing the two features.
+        message = [
+            ':هناك طريقتان للاستمتاع بالمقطع بلا موسيقا\n',
+            'خيار المشاهدة الفورية: يعني مشاهدة المقطع على',
+            '.شكل لقطات نقية لكي لا تنتظر فترة طويلة\n',
+            'يتم حفظ المقطع خاليا من الموسيقا بعد الانتهاء',
+            '.من مشاهدته كاملا\n',
+            'أما خيار حفظ المقطع فهو فقط يحفظ المقطع',
+            '.بعد تنقيته من الموسيقا'
+        ]
+        self.guiding_label.config(justify='right')
+        self.update_guiding_label('\n'.join(message))
+
+        self.direct_display_btn = Button(self, BTN_STYLE, text='مشاهدة فورية', command=lambda: self.go_to_option('show'))
         self.direct_display_btn.pack(BTN_PACKING)
 
-        self.process_video_btn = Button(
-            self,
-            text='حفظ المقطع بلا موسيقا',
-            bg=BTN_BG,
-            fg=FG,
-            command=lambda: self.go_to_option('save')
-        )
-        self.process_video_btn.pack(BTN_PACKING, ipadx=24, pady=10)
+        self.save_video_btn = Button(self, BTN_STYLE, text='حفظ المقطع', command=lambda: self.go_to_option('save'))
+        self.save_video_btn.pack(BTN_PACKING, ipadx=24, pady=10)
 
     def go_to_option(self, option: str):
-        for btn in (self.direct_display_btn, self.process_video_btn):
+        '''
+        It orients the program depending on the method that the user selected.
+        '''
+        self.guiding_label.config(justify='center')
+        for btn in (self.direct_display_btn, self.save_video_btn):
             btn.forget()
 
         if option == 'show':
-            # self.clip_options_thread = Thread(target=self.show_watching_options, daemon=True)
-            # self.clip_options_thread.start()
             self.show_watching_options()
 
         elif option == 'save':
@@ -316,51 +340,65 @@ class App(Tk):
             Thread(target=self.show_finishing_options, daemon=True).start()
 
     def show_watching_options(self):
+        '''
+        It shows the options of the watching method.\n
+        As you selected the 'watching' method, this function will be called.\n
+        '''
         self.label_packing['pady'] = (50, 30)
         self.update_guiding_label('حدد إعدادات المشاهدة')
-        
+
+        COMBOBOX_STYLE = {'state': 'readonly', 'justify': 'center'}
         self.clip_dimensions = StringVar()
+        self.clip_duration = StringVar()
+
         self.dimensions_combobox = Combobox(
             self,
-            textvariable=self.clip_dimensions,
             state='readonly',
-            justify='center'
+            justify='center',
+            textvariable=self.clip_dimensions
         )
+        self.duration_combobox = Combobox(
+            self,
+            state='readonly',
+            justify='center',
+            textvariable=self.clip_duration
+        )
+        
         self.dimensions_combobox.config(
             values=(
                 'اختر أبعاد اللقطات',
-                '720x1280',
-                '1080x1920',
+                '1280x720',
+                '1920x1080',
                 'وضع الشاشة الكاملة'
             )
         )
-        self.dimensions_combobox.current(0)
-        self.dimensions_combobox.pack()
-
-        self.clip_duration = StringVar()
-        self.duration_combobox = Combobox(
-            self,
-            textvariable=self.clip_duration,
-            state='readonly',
-            justify='center'
-        )
         self.duration_combobox.config(
             values=(
-                'اختر مدة اللقطات',
+                'اختر مدة كل اللقطات',
                 '10s',
                 '60s',
             )
         )
+
+        # set the first one as a placeholder
+        self.dimensions_combobox.current(0)
         self.duration_combobox.current(0)
+
+        self.dimensions_combobox.pack()
         self.duration_combobox.pack(pady=20)
 
-        self.save_options_btn = Button(self, text='بدء المشاهدة', bg=BTN_BG, fg=FG, command=self.check_selection)
+        self.save_options_btn = Button(self, BTN_STYLE, text='بدء المشاهدة', command=self.check_selection)
         self.save_options_btn.pack(ipadx=30, ipady=3)
     
     def check_selection(self):
+        '''
+        It checks if all settings have been selected.\n
+        If not, it will call `showerror()`.
+        '''
         dimensions = self.clip_dimensions.get()
         duration = self.clip_duration.get()
 
+        # if all settings have been set
         if dimensions != 'اختر أبعاد اللقطات' and duration != 'اختر مدة اللقطات':
             self.gui_preparation('select clip options')
             self.processing_thread = Thread(
@@ -373,13 +411,17 @@ class App(Tk):
             showerror('خطأ', 'يجب تحديد جميع الإعدادات')
         
     def show_finishing_options(self):
+        '''
+        It shows the end screen, and offers either to `open_video()`, or to `open_video_folder()`.
+        '''
         self.processing_thread.join()
+        
         self.progress_bar.forget()
         self.label_packing['pady'] = (50, 50)
         self.update_guiding_label('..انتهت المعالجة بنجاح')
 
-        Button(self, text='فتح المقطع', bg=BTN_BG, fg=FG, command=self.open_video).pack(BTN_PACKING)
-        Button(self, text='فتح موقع المقطع', bg=BTN_BG, fg=FG,
+        Button(self, BTN_STYLE, text='فتح المقطع', command=self.open_video).pack(BTN_PACKING)
+        Button(self, BTN_STYLE, text='فتح موقع المقطع',
             command=self.open_video_folder).pack(BTN_PACKING, pady=10)
         
         self.video_name = self.rename_video_to('original')
@@ -389,11 +431,11 @@ class App(Tk):
         It prepares the GUI according to the current step.
 
         step_name should be one of the following:
-            - 'select clip options'
-            - 'slice video into clips'
-            - 'extract audio'
-            - 'separate music'
-            - 'merge media'
+            - `'select clip options'`
+            - `'slice video into clips'`
+            - `'extract audio'`
+            - `'separate music'`
+            - `'merge media'`
         '''
         appropriate_step_names = (
             'select clip options',
@@ -436,25 +478,37 @@ class App(Tk):
             self.update_guiding_label('..دمج الصوت مع المقطع')
 
     def open_video(self):
+        '''
+        It opens the video through it's saved name in `self.name_cases['music removed']`.
+        '''
         os.startfile(self.name_cases['music removed'])
 
     def open_video_folder(self):
+        '''
+        It opens the video folder through the video's saved name in `self.name_cases['original']`
+        '''
         filename = self.name_cases['original']
         folder = filename[:filename.rfind('/')]
         os.startfile(folder)
 
     def rename_video_to(self, case: str):
         '''
-        In all 2 cases, this method will be used to rename video file to/from 'underscored' and 'original'
+        It renames video file to/from `'underscored'` and `'original'`.
         '''
-        if case == 'original':
-            os.rename(self.name_cases['underscored'], self.name_cases[case])
-        elif case == 'underscored':
-            os.rename(self.name_cases['original'], self.name_cases[case])
+        try:
+            if case == 'original':
+                os.rename(self.name_cases['underscored'], self.name_cases[case])
+            elif case == 'underscored':
+                os.rename(self.name_cases['original'], self.name_cases[case])
+        except FileExistsError:
+            print('\n\n\nIt seems the filename is already underscored\n\n\n')
         
         return self.name_cases[case]
 
     # def delete_temporary_files(self, there_is_clips=False):
+    #     '''
+    #     It removes any temporary files that used through any process.  
+    #     '''
     #     os.system('cls')
     #     os.remove('vocals.wav')
     #     os.remove(self.audio_name)
@@ -477,10 +531,16 @@ class App(Tk):
 
         
     def update_progress_bar(self, percentage: float):
+        '''
+        It updates the progress bar percentage.
+        '''
         self.progress_bar.config(value=percentage)
         self.update()
 
     def update_guiding_label(self, text: str):
+        '''
+        It updates the text of the guiding label.
+        '''
         self.guiding_label.forget()
         self.guiding_label.config(text=text)
         self.guiding_label.pack(self.label_packing)
